f721e7ad58f68fce8defe1bd6ba70c33304b33d6
Karolina <karolinasingulani2@gmail.com>
2023-10-20 18:44:31 -0300

    Category

--- src/main/java/com/WebService/project/config/TestConfig.java
+++ src/main/java/com/WebService/project/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.webservice.project.entities.Category;
 import com.webservice.project.entities.Order;
 import com.webservice.project.entities.User;
 import com.webservice.project.entities.enums.OrderStatus;
+import com.webservice.project.repositories.CategoryRepository;
 import com.webservice.project.repositories.OrderRepository;
 import com.webservice.project.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner{
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+    
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
@@ -35,6 +40,12 @@ public class TestConfig implements CommandLineRunner{
         Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        Category cat1 = new Category(null, "Electronics"); 
+        Category cat2 = new Category(null, "Games"); 
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
     }
 }
