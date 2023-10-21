576671c175ca4e55cb4cdbe97adbd38c8db82620
Karolina <karolinasingulani2@gmail.com>
2023-10-21 17:29:43 -0300

    Exceptio handling

---
+++ src/main/java/com/WebService/project/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.webservice.project.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id){
+        super("Resource not found, Id: " + id);
+    }
+}
